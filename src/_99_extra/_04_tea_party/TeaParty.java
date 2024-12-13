5f86352fd2018cfb7374ba3881023f65a7171c0b
Aarit Jayaswal <aaritrj@gmail.com>
2024-12-12 17:03:11 -0800

    12/12/24 Today, I did tea maker, backpack, and 99 extra tea party

---
+++ src/_99_extra/_04_tea_party/TeaParty.java
@@ -0,0 +1,12 @@
+package _99_extra._04_tea_party;
+
+	public class TeaParty {
+	    public String welcome(String name, boolean isWoman, boolean isWeirdo) {
+	        if(isWoman == true) {
+	        	
+	        }
+	        return "hi";
+	    }
+	}
+	
+
